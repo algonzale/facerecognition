49270a604dc703ac4100155ba1b2f2e4c350e822
Alejandro Jimenez <ajim4268@github.com>
2019-05-02 22:06:48 -0600

    Updates

---
+++ precache-manifest.59085394546215a9cc263f61e8d64797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c28bb55de6f42105bee",
+    "url": "/facerecognition/static/js/runtime~main.2c28bb55.js"
+  },
+  {
+    "revision": "1b7e060f6d9dc7c85dbc",
+    "url": "/facerecognition/static/js/main.1b7e060f.chunk.js"
+  },
+  {
+    "revision": "df8ece78f04ecb6a3544",
+    "url": "/facerecognition/static/js/2.df8ece78.chunk.js"
+  },
+  {
+    "revision": "1b7e060f6d9dc7c85dbc",
+    "url": "/facerecognition/static/css/main.2d36cde7.chunk.css"
+  },
+  {
+    "revision": "df8ece78f04ecb6a3544",
+    "url": "/facerecognition/static/css/2.c4129609.chunk.css"
+  },
+  {
+    "revision": "434020f691a6495ac1de46901c43bd5c",
+    "url": "/facerecognition/index.html"
+  }
+];
